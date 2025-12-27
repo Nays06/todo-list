df35943b2f6d45bfcac5eb75bcf6ebd69b38b91c
Nays06 <adam.ovsh00@mail.ru>
2025-12-27 15:20:59 +0300

    commit 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,48 @@
-import React, { useState } from 'react';
-import './App.css';
-import useStore from './stores/index.ts';
+import React, { useState } from "react";
+import "./App.css";
+import useStore from "./stores/index.ts";
 
 function App() {
-  const { items, addToDos, changeItemCheck, reset } = useStore()
-  const [title, setTitle] = useState<string>('')
-  const [check, setCheck] = useState<boolean>(false)
+  const { items, addToDos, changeItemCheck, reset } = useStore();
+  const [title, setTitle] = useState<string>("");
+  const [check, setCheck] = useState<boolean>(false);
 
   function changeTitle(event: React.ChangeEvent<HTMLInputElement>) {
     setTitle(event.target.value);
   }
   function changeCheck() {
-    setCheck(!check)
+    setCheck(!check);
   }
   function addToDo() {
-    addToDos({ title: title, check: check })
-    setTitle('')
-    setCheck(false)
+    addToDos({ title: title, check: check });
+    setTitle("");
+    setCheck(false);
   }
-  function changeCheckForItem(itemId:string) {
-    changeItemCheck(itemId)
+  function changeCheckForItem(itemId: string) {
+    changeItemCheck(itemId);
   }
 
   return (
     <main>
       <div>
-        <input value={title} onChange={changeTitle} type="text" placeholder='Название' />
+        <input value={title} onChange={changeTitle} type="text" placeholder="Название" />
         <input checked={check} onChange={changeCheck} type="checkbox" />
         <button onClick={addToDo}>Добавить</button>
         <button onClick={reset}>Reset</button>
       </div>
 
-      {items.map((item) => <li className={item.check ? "done" : ""} key={item.id}>{item.title} <input checked={item.check} onClick={() => changeCheckForItem(item.id)} type="checkbox" /></li>)}
+      {items.map((item) => (
+        <li className={item.check ? "done" : ""} key={item.id}>
+          {item.title}{" "}
+          <input
+            checked={item.check}
+            onClick={() => item.id && changeCheckForItem(item.id)}
+            type="checkbox"
+          />
+        </li>
+      ))}
     </main>
-  )
+  );
 }
 
 export default App;
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,27 +1,31 @@
 import { create } from 'zustand'
 
-interface store {
-  items: Array<{
-    id: string,
-    title: string,
-    check: boolean
-  }>,
-  addToDos: any,
-  changeItemCheck: any,
+type item = {
+  id?: string ,
+  title: string,
+  check: boolean
+}
+
+interface Store {
+  items: item[],
+  addToDos: (item: item ) => void,
+  changeItemCheck: (itemId: string) => void,
   reset: () => void,
 }
 
-const useStore = create<store>((set) => ({
+const useStore = create<Store>((set) => ({
   items: [
     { id: '1', title: 'Задача №1', check: true },
     { id: '2', title: 'Задача №2', check: true },
     { id: '3', title: 'Задача №3', check: false },
   ],
-  addToDos: (item: any) => {
+  addToDos: (item) => {
     item.id = Date.now().toString()
-    set((state) => ({ items: [...state.items, item] }))
+    if(item.id){
+      set((state) => ({ items: [...state.items, item] }))
+    }
   },
-  changeItemCheck: (itemId: string) => {
+  changeItemCheck: (itemId) => {
     set((state) => ({
       items: state.items.map(item =>
         item.id === itemId
